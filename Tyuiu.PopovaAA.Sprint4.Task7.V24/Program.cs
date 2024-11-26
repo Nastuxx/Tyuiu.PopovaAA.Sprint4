cf4b11696e3a92f192a2063a51844b5935849f03
Tyuiu.PopovaAA <Tyuiu.PopovaAA@outlook.com>
2024-11-26 21:19:52 +0500

    7

---
+++ Tyuiu.PopovaAA.Sprint4.Task7.V24/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.PopovaAA.Sprint4.Task7.V24.Lib;
+namespace Tyuiu.PopovaAA.Sprint4.Task7.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int rows = 5;
+            int columns = 3;
+            string str = "651827384219648";
+            int[,] numbers = new int[rows, columns];
+
+
+            Console.Title = "Спринт #4 | Выполнила Попова А.А. | РППб-24-1";
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                 *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                   *");
+            Console.WriteLine("* Задание #7                                                                *");
+            Console.WriteLine("* Вариант #24                                                               *");
+            Console.WriteLine("* Выполнила: Попова Анастасия Алексеевна | РППб-24-1                        *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 651827384219648. Преобразуйте ее в      *");
+            Console.WriteLine("* матрицу 5 на 3 и подсчитайте произведение четных чисел.                   *");
+            Console.WriteLine("*                                                                           *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("*****************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("*****************************************************************************");
+
+            int res = ds.Calculate(rows, columns, str);
+
+            Console.WriteLine("Произведение четных чисел = " + res);
+            Console.ReadKey();
+        }
+    }
+}
